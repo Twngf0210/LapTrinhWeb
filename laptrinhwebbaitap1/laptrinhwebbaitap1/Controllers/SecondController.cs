97f6e38e521f39534fc0d80d3b869ff227abc029
Twngf <caubengungo6454@gmail.com>
2025-08-29 01:35:07 +0700

    Thêm lại code bài tập 1 (fix submodule)

---
+++ laptrinhwebbaitap1/laptrinhwebbaitap1/Controllers/SecondController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace laptrinhwebbaitap1.Controllers
+{
+    public class SecondController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            ViewData["Name"] = "RTX 3060";
+            ViewData["NSX"] = "2021";
+            ViewData["Owner"] = "NVIDIA";
+            return View();
+        }
+    }
+}
